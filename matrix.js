beaee22895df22e0b717bb983f2a01d38912492f
Christopher Broome <ChrisBroome7@gmail.com>
2012-08-09 02:48:45 -0400

    Refactor matrix prototype and tests

--- matrix.js
+++ matrix.js
@@ -1,153 +1,188 @@
 // @description: creates a matrix from the given 2 dimensional array
 function Matrix( els ) {
+  // @description: returns the prototype from which Matrix is extended
+  function proto() {
+    function defaults() {
+      var defs = {
+        a: [],
+        size: {
+          rows: 0,
+          cols: 0
+        },
+        name: ''
+      };
+      if( typeof els === 'object' && els.constructor === Array ) {
+        defs.a = els.slice();
+        defs.size.rows = defs.a.length;
+        defs.size.cols = defs.a[0].length;
+      };
+      return defs;
+    }
 
-  var defaults = init(els);
-  var a = defaults.a,
-      size = defaults.size;
-
-  this.els = a;
-  this.size = size;
-  this.row = row;
-  this.col = col;
-  this.at = at;
-  this.rows = rows;
-  this.cols = cols;
-  this.add = add;
-  this.multiply = multiply;
-  this.concat = concat;
-
-  // @description: initializes default values from the given object
-  function init( e ) {
-    var defs = {
-      a: [],
-      size: {
-        rows: 0,
-        cols: 0
+    function addMethods( o ) {
+      function addMethod( name, func, fmt ) {
+        o[name] = func;
+        o[name].fmt = fmt;
       }
-    };
-    if( typeof e === 'object' && e.constructor === Array ) {
-      defs.a = e.slice();
-      defs.size.rows = defs.a.length;
-      defs.size.cols = defs.a[0].length;
-    };
-    return defs;
-  }
-
-  // @description: gets a column vector at the specified row
-  function row( i ) {
-    return a[ i ].slice();
-  }
-
-  // @description: gets a row vector at the specified column
-  function col( i ) {
-    return a.map( function(v) { return v[i]; } ).slice();
-  }
-
-  // @description: gets the element at the specified row, col
-  function at( row, col ) {
-    return a[ row ][ col ];
-  }
-
-  // @description: returns an array of row vectors
-  function rows() {
-    return a.slice();
-  }
-
-  // @description: returns an array of column vectors
-  function cols() {
-    var c, x = [];
-    for( c = 0; c < size.cols; c++ ) {
-      x.push( col(c) );
+      addMethod( 'elements', elements, '{0}' );
+      addMethod( 'size', size, '|{0}|' );
+      addMethod( 'rows', rows, 'rows' );
+      addMethod( 'cols', cols, 'cols' );
+      addMethod( 'row', row, 'row{0}' );
+      addMethod( 'col', col, 'col{0}' );
+      addMethod( 'at', at, '[{0},{1}]' );
+      addMethod( 'add', add, '+{0}' );
+      addMethod( 'multiply', multiply, '*{0}' );
+      addMethod( 'concat', concat, '&{0}' );
+      addMethod( 'toString', toString, '' );
+      return o;
     }
-    return x;
-  }
 
-  // @descprition: adds another matrix or constant to this matrix and returns the result
-  function add( m ) {
-    if( typeof m === 'number' ) {
-      return add_constant( m );
+    function isMatrix( o ) {
+      return typeof o === 'object';
     }
-    else if( typeof m === 'object' && m.constructor === Matrix ) {
-      return add_matrix( m );
+
+    // @description: gets the rows
+    function elements() { return a.slice(); }
+    function size() { return size; }
+
+    // @description: gets a column vector at the specified row
+    function row( i ) {
+      return a[ i ].slice();
     }
-    else {
-      return m;
+
+    // @description: gets a row vector at the specified column
+    function col( i ) {
+      return a.map( function(v) { return v[i]; } ).slice();
     }
-  }
 
-  // @description: multiplies another matrix or constant to this matrix and returns the result
-  function multiply( m ) {
-    if( typeof m === 'number' ) {
-      return multiply_constant( m );
+    // @description: gets the element at the specified row, col
+    function at( row, col ) {
+      return a[ row ][ col ];
     }
-    else if( typeof m === 'object' && m.constructor === Matrix ) {
-      return multiply_matrix( m );
+
+    // @description: returns an array of row vectors
+    function rows() {
+      return a.slice();
     }
-    else {
-      return m;
+
+    // @description: returns an array of column vectors
+    function cols() {
+      var c, x = [];
+      for( c = 0; c < size.cols; c++ ) {
+        x.push( col( c ) );
+      }
+      return x;
     }
-  }
 
-  function iterate( cb ) {
-    return a.map( function( row, r_index ) {
-      return row.map( function( value, c_index ) {
-          return cb( value, c_index, r_index, row );
-        } ).slice();
-    } ).slice();
-  }
+    // @descprition: adds another matrix or constant to this matrix and returns the result
+    function add( m ) {
+      if( typeof m === 'number' ) {
+        return add_constant( m );
+      }
+      else if( isMatrix( m ) ) {
+        return add_matrix( m );
+      }
+      else {
+        return m;
+      }
+    }
 
-  function add_constant( n ) {
-    return new Matrix(
-      iterate( function( v ) {
-        return v + n;
-      } )
-    );
-  }
+    // @description: multiplies another matrix or constant to this matrix and returns the result
+    function multiply( m ) {
+      if( typeof m === 'number' ) {
+        return multiply_constant( m );
+      }
+      else if( isMatrix( m ) ) {
+        return multiply_matrix( m );
+      }
+      else {
+        return m;
+      }
+    }
 
-  function add_matrix( m ) {
-    return new Matrix(
-      iterate( function( v, c, r, row ) {
-        return v + m.at( r, c );
-      } )
-    );
-  }
+    function iterate( cb ) {
+      return a.map( function( row, r_index ) {
+        return row.map( function( value, c_index ) {
+            return cb( value, c_index, r_index, row );
+          } ).slice();
+      } ).slice();
+    }
 
-  function multiply_constant( n ) {
-    return new Matrix(
-      iterate( function( v ) {
-        return v * n;
-      } )
-    );
-  }
+    function add_constant( n ) {
+      return new Matrix(
+        iterate( function( v ) {
+          return v + n;
+        } )
+      );
+    }
 
-  function multiply_matrix( m ) {
-    if( size.cols != m.size.rows ) throw new Error('size.cols must equal m.size.rows');
-    var x = rows().map( function( row_r, r ) {
-      return m.cols().map( function( col_c, c ) {
-        return vector_dot( row_r, col_c );
+    function add_matrix( m ) {
+      return new Matrix(
+        iterate( function( v, c, r, row ) {
+          return v + m.at( r, c );
+        } )
+      );
+    }
+
+    function multiply_constant( n ) {
+      return new Matrix(
+        iterate( function( v ) {
+          return v * n;
+        } )
+      );
+    }
+
+    function multiply_matrix( m ) {
+      if( size.cols != m.size.rows ) throw new Error('size.cols must equal m.size.rows');
+      var x = rows().map( function( row_r, r ) {
+        return m.cols().map( function( col_c, c ) {
+          return vector_dot( row_r, col_c );
+        }).slice();
       }).slice();
-    }).slice();
-    return new Matrix( x );
+      return new Matrix( x );
+    }
+
+    function vector_dot( v1, v2 ) {
+      if( v1.length != v2.length ) throw new Error('Vectors are not the same size.');
+      return v1.map( function( v, k ) {
+        return v * v2[k];
+      } ).reduce( function( va, vi ) {
+        va += vi;
+        return va;
+      } );
+    }
+
+    function concat( m ) {
+      if( size.rows != m.size.rows )
+        throw new Error('Matrices have different number of rows');
+      var x = rows().map( function( row_r, r ) {
+        return row_r.concat( m.row(r) );
+      } ).slice();
+      return new Matrix( x );
+    }
+
+    function toString() {
+      return rows().reduce( function( ar, row_r, r ) {
+        return ar + row_r.reduce( function( ac, col_c, c ) {
+          return ac + ' ' + col_c;
+        }, '\n' );
+      }, '' );
+    }
+
+    // define the prototype and required variables
+    var prot = defaults(),
+        a = prot.a,
+        size = prot.size;
+
+    // add methods to the defaults
+    addMethods( prot );
+
+    // return the defaults
+    return prot;
   }
 
-  function vector_dot( v1, v2 ) {
-    if( v1.length != v2.length ) throw new Error('Vectors are not the same size.');
-    return v1.map( function( v, k ) {
-      return v * v2[k];
-    } ).reduce( function( va, vi ) {
-      va += vi;
-      return va;
-    } );
-  }
-
-  function concat( m ) {
-    if( size.rows != m.size.rows )
-      throw new Error('Matrices have different number of rows');
-    var x = rows().map( function( row_r, r ) {
-      return row_r.concat( m.row(r) );
-    } ).slice();
-    return new Matrix( x );
-  }
+  return new proto();
 }
 
 Matrix.zero = new Matrix();
--- test.js
+++ test.js
@@ -1,90 +1,124 @@
 var Matrix = require('./matrix');
 var rat = require('./rational');
 
-var a = new Matrix( [ [1, 2], [3, 4] ] );
-console.log('a: ', a.els);
-var b = new Matrix( [ [5, 6], [7, 8] ] );
-console.log('b: ', b.els);
-var c = a.add(32);
-console.log('c: ', c.els);
-var d = a.add(b);
-console.log('d: ', d.els);
-var e = a.multiply( 10 );
-console.log('e: ', e.els);
-var f = a.multiply( b );
-console.log('f: ', f.els);
-
-var g = new Matrix( [ [7, 8, 9], [10, 11, 12] ] );
-console.log('g: ', g.els);
-var h = a.multiply( g );
-console.log('h = ag: ', h.els);
-
-console.log('a.rows(): ', a.rows());
-console.log('a.cols(): ', a.cols());
-
-console.log('g.rows(): ', g.rows());
-console.log('g.cols(): ', g.cols());
-
-var x = new Matrix( [
-  [7, 3, 5, 6],
-  [1, 2, 3, 2],
-  [8, 4, 3, 3]
-] );
-
-console.log('x: ', x.els);
-
-var y = new Matrix( [
-  [1, 3, 2, 3, 6],
-  [8, 3, 2, 4, 2],
-  [2, 6, 2, 3, 7],
-  [4, 9, 6, 3, 7]
-] );
-
-console.log('y: ', y.els);
-
-var r = x.multiply(y);
-console.log('r = xy: ', r.els);
-
-var z = a.concat(g);
-console.log('z = a concat g: ', z.els );
-
-function identities( n ) {
-  var r = [], i;
-  for( i = 0; i <= n; i++ ) {
-    r.push(Matrix.identity(i));
+function matrixTests() {
+  function setup() {
+    var m = {
+      a: new Matrix([
+        [1, 2],
+        [3, 4]
+      ]),
+      b: new Matrix([
+        [5, 6],
+        [7, 8]
+      ]),
+      g: new Matrix([
+        [ 7,  8,  9],
+        [10, 11, 12]
+      ]),
+      x: new Matrix([
+        [7, 3, 5, 6],
+        [1, 2, 3, 2],
+        [8, 4, 3, 3]
+      ]),
+      y: new Matrix([
+        [1, 3, 2, 3, 6],
+        [8, 3, 2, 4, 2],
+        [2, 6, 2, 3, 7],
+        [4, 9, 6, 3, 7]
+      ])
+    };
+    return m;
   }
-  return r;
+
+  function output( desc, m, func, lhs, rhs ) {
+    function valOrName( o ) {
+      return typeof o !== 'undefined' && typeof o.name === 'string' ? o.name : o;
+    }
+    // var f = func.fmt.slice();
+    // var i = f.replace('{0}', valOrName(lhs)).replace('{1}', valOrName(rhs));
+    // if( typeof rhs !== 'undefined' ) {
+    //   console.log( i + ': ', lhs[func]( rhs ) );
+    // } else {
+    //   console.log( i + ': ', lhs[func]() );
+    // }
+
+    console.log( desc + ' = ', m.toString() );
+  }
+
+  function identities( n ) {
+    var r = [], i;
+    for( i = 0; i <= n; i++ ) {
+      r.push( Matrix.identity( i ) );
+    }
+    return r;
+  }
+
+  function run() {
+    var matrices = setup();
+
+    var a = matrices.a;
+    var b = matrices.b;
+    var g = matrices.g;
+    var x = matrices.x;
+    var y = matrices.y;
+
+    output( 'a', a );
+    output( 'b', b );
+    output( 'a + 32', a.add( 32 ) );
+    output( 'a * 10', a.multiply( 10 ) );
+    output( 'a + b', a.add( b ) );
+    output( 'a * b', a.multiply( b ) );
+    output( 'g', g );
+    output( 'a * g', a.multiply( g ) );
+    output( 'x', x );
+    output( 'y', y );
+    output( 'x * y', x.multiply( y ) );
+    output( 'a & g', a.concat( g ) );
+
+    identities(5).map( function( v, k ) {
+      output('I('+k+')', v );
+    } );
+  }
+
+  run();
 }
 
-identities(5).map(function(v) {
-  console.log(v.els);
-});
+function rationalTests() {
+  function gcdTest( a, b ) {
+    console.log('gcd( %d, %d ) = %d', a, b, rat.gcd( a,b ) );
+  }
 
-function gcdTest(a, b) {
-  console.log('gcd( %d, %d ) = %d', a, b, rat.gcd(a,b));
+  function ratTest( a, b ) {
+    console.log('Rational( %d, %d ) = %s', a, b, rat.Rational( a,b ) );
+  }
+
+  var pairs = [
+    [ 142, 22 ],
+    [ 40, 35 ],
+    [ 77, 30 ],
+    [ 5325123533, 5325123533 ],
+    [ 5325123530, 123442 ],
+    [ 2*3*5*7, 5*7*11*13 ],
+    [ 1989, 867 ],
+    [ 2*3*5*7*11*13*17, 11*13*17 ],
+    [ 364117.4207441509, 241980.82787916064 ]
+  ];
+
+  pairs.forEach( function( p ) {
+    var a = p[0], b = p[1];
+    gcdTest( a, b );
+  });
+
+  pairs.forEach( function( p ) {
+    var a = p[0], b = p[1];
+    ratTest( a, b );
+  });
 }
 
-function ratTest(a, b) {
-  console.log('Rational( %d, %d ) = %s', a, b, rat.Rational(a,b));
+function run() {
+  matrixTests();
+  rationalTests();
 }
 
-var pairs = [
-  [ 142, 22 ],
-  [ 40, 35 ],
-  [ 77, 30 ],
-  [ 5325123533, 5325123533 ],
-  [ 5325123530, 123442 ],
-  [ 2*3*5*7, 5*7*11*13 ],
-  [ 1989, 867 ],
-  [ 2*3*5*7*11*13*17, 11*13*17 ],
-  [ 364117.4207441509, 241980.82787916064 ]
-];
-
-pairs.forEach( function( p ) {
-  var a = p[0], b = p[1];
-  gcdTest( a, b );
-});
-pairs.forEach( function( p ) {
-  var a = p[0], b = p[1];
-  ratTest( a, b );
-});
+run();
