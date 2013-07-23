cd8d70cf3de8c25561e06cad9fed6ff73c646892
Christopher Broome <ChrisBroome7@gmail.com>
2013-07-23 01:43:07 -0400

    Refactor all methods out of the constructor

--- matrix.js
+++ matrix.js
@@ -1,189 +1,20 @@
 // @description: creates a matrix from the given 2 dimensional array
 function Matrix( els ) {
-  // @description: returns the prototype from which Matrix is extended
-  function proto() {
-    function defaults() {
-      var defs = {
-        a: [],
-        size: {
-          rows: 0,
-          cols: 0
-        },
-        name: ''
-      };
-      if( typeof els === 'object' && els.constructor === Array ) {
-        defs.a = els.slice();
-        defs.size.rows = defs.a.length;
-        defs.size.cols = defs.a[0].length;
-      };
-      return defs;
+  if( Array.isArray(els) ) {
+    var a = els.slice(0);
+    this.a = a;
+    this.R = a.length;
+    if( this.R > 0 ) {
+      this.C = this.a[0].length;
     }
-
-    function addMethods( o ) {
-      function addMethod( name, func, fmt ) {
-        o[name] = func;
-        o[name].fmt = fmt;
-      }
-      addMethod( 'elements', elements, '{0}' );
-      addMethod( 'size', size, '|{0}|' );
-      addMethod( 'rows', rows, 'rows' );
-      addMethod( 'cols', cols, 'cols' );
-      addMethod( 'row', row, 'row{0}' );
-      addMethod( 'col', col, 'col{0}' );
-      addMethod( 'at', at, '[{0},{1}]' );
-      addMethod( 'add', add, '+{0}' );
-      addMethod( 'multiply', multiply, '*{0}' );
-      addMethod( 'concat', concat, '&{0}' );
-      addMethod( 'toString', toString, '' );
-      return o;
-    }
-
-    function isMatrix( o ) {
-      return typeof o === 'object';
-    }
-
-    // @description: gets the rows
-    function elements() { return a.slice(); }
-    function size() { return size; }
-
-    // @description: gets a column vector at the specified row
-    function row( i ) {
-      return a[ i ].slice();
-    }
-
-    // @description: gets a row vector at the specified column
-    function col( i ) {
-      return a.map( function(v) { return v[i]; } ).slice();
-    }
-
-    // @description: gets the element at the specified row, col
-    function at( row, col ) {
-      return a[ row ][ col ];
-    }
-
-    // @description: returns an array of row vectors
-    function rows() {
-      return a.slice();
-    }
-
-    // @description: returns an array of column vectors
-    function cols() {
-      var c, x = [];
-      for( c = 0; c < size.cols; c++ ) {
-        x.push( col( c ) );
-      }
-      return x;
-    }
-
-    // @descprition: adds another matrix or constant to this matrix and returns the result
-    function add( m ) {
-      if( typeof m === 'number' ) {
-        return add_constant( m );
-      }
-      else if( isMatrix( m ) ) {
-        return add_matrix( m );
-      }
-      else {
-        return m;
-      }
-    }
-
-    // @description: multiplies another matrix or constant to this matrix and returns the result
-    function multiply( m ) {
-      if( typeof m === 'number' ) {
-        return multiply_constant( m );
-      }
-      else if( isMatrix( m ) ) {
-        return multiply_matrix( m );
-      }
-      else {
-        return m;
-      }
-    }
-
-    function iterate( cb ) {
-      return a.map( function( row, r_index ) {
-        return row.map( function( value, c_index ) {
-            return cb( value, c_index, r_index, row );
-          } ).slice();
-      } ).slice();
-    }
-
-    function add_constant( n ) {
-      return new Matrix(
-        iterate( function( v ) {
-          return v + n;
-        } )
-      );
-    }
-
-    function add_matrix( m ) {
-      return new Matrix(
-        iterate( function( v, c, r, row ) {
-          return v + m.at( r, c );
-        } )
-      );
-    }
-
-    function multiply_constant( n ) {
-      return new Matrix(
-        iterate( function( v ) {
-          return v * n;
-        } )
-      );
-    }
-
-    function multiply_matrix( m ) {
-      if( size.cols != m.size.rows ) throw new Error('size.cols must equal m.size.rows');
-      var x = rows().map( function( row_r, r ) {
-        return m.cols().map( function( col_c, c ) {
-          return vector_dot( row_r, col_c );
-        }).slice();
-      }).slice();
-      return new Matrix( x );
-    }
-
-    function vector_dot( v1, v2 ) {
-      if( v1.length != v2.length ) throw new Error('Vectors are not the same size.');
-      return v1.map( function( v, k ) {
-        return v * v2[k];
-      } ).reduce( function( va, vi ) {
-        va += vi;
-        return va;
-      } );
-    }
-
-    function concat( m ) {
-      if( size.rows != m.size.rows )
-        throw new Error('Matrices have different number of rows');
-      var x = rows().map( function( row_r, r ) {
-        return row_r.concat( m.row(r) );
-      } ).slice();
-      return new Matrix( x );
-    }
-
-    function toString() {
-      return rows().reduce( function( ar, row_r, r ) {
-        return ar + row_r.reduce( function( ac, col_c, c ) {
-          return ac + ' ' + col_c;
-        }, '\n' );
-      }, '' );
-    }
-
-    // define the prototype and required variables
-    var prot = defaults(),
-        a = prot.a,
-        size = prot.size;
-
-    // add methods to the defaults
-    addMethods( prot );
-
-    // return the defaults
-    return prot;
   }
-
-  return new proto();
 }
+Matrix.prototype = addMethods({
+  a: [],
+  rows: 0,
+  cols: 0,
+  name: ''
+});
 
 Matrix.zero = new Matrix();
 
@@ -200,4 +31,218 @@ Matrix.identity = function( n ) {
   return new Matrix( m );
 }
 
+function addMethod( name, func, fmt ) {
+  this[name] = func;
+  this[name].fmt = fmt;
+}
+
+function addMethods( o ) {
+  var f = addMethod.bind(o);
+  f( 'elements', elements, '{0}' );
+  f( 'size', size, '|{0}|' );
+  f( 'rows', rows, 'rows' );
+  f( 'cols', cols, 'cols' );
+  f( 'row', row, 'row{0}' );
+  f( 'col', col, 'col{0}' );
+  f( 'at', at, '[{0},{1}]' );
+  f( 'add', add, '+{0}' );
+  f( 'addConstant', addConstant, '+{0}');
+  f( 'addMatrix', addMatrix, '+{0}');
+  f( 'multiply', multiply, '*{0}' );
+  f( 'multiplyConstant', multiplyConstant, '*{0}' );
+  f( 'multiplyMatrix', multiplyMatrix, '*{0}' );
+  f( 'concat', concat, '&{0}' );
+  f( 'flatten', flatten, '' );
+  f( 'iterate', iterate, '' );
+  f( 'toString', toString, '' );
+  return o;
+}
+
+function isMatrix( o ) {
+  return typeof o === 'object';
+}
+
+// @description: gets the rows
+function elements() {
+  return this.a.slice();
+}
+
+function size() {
+  return size;
+}
+
+// @description: gets a column vector at the specified row
+function row( i ) {
+  return this.a[i].slice();
+}
+
+// @description: gets a row vector at the specified column
+function col( i ) {
+  return this.a.map( colSelector.bind(i) ).slice();
+}
+
+function colSelector(v) {
+  return v[this];
+}
+
+// @description: gets the element at the specified row, col
+function at( row, col ) {
+  return this.a[ row ][ col ];
+}
+
+// @description: returns an array of row vectors
+function rows() {
+  return this.a.slice();
+}
+
+// @description: returns an array of column vectors
+function cols() {
+  var c, x = [];
+  for( c = 0; c < this.C; c++ ) {
+    x.push( this.col( c ) );
+  }
+  return x;
+}
+
+// @descprition: adds another matrix or constant to this matrix and returns the result
+function add( m ) {
+  if( typeof m === 'number' ) {
+    return this.addConstant( m );
+  }
+  else if( isMatrix( m ) ) {
+    return this.addMatrix( m );
+  }
+  else {
+    return m;
+  }
+}
+
+// @description: multiplies another matrix or constant to this matrix and returns the result
+function multiply( m ) {
+  if( typeof m === 'number' ) {
+    return this.multiplyConstant( m );
+  }
+  else if( isMatrix( m ) ) {
+    return this.multiplyMatrix( m );
+  }
+  else {
+    return m;
+  }
+}
+
+function iterate( cb ) {
+  return this.a.map( function( row, r_index ) {
+    return row.map( function( value, c_index ) {
+        return cb( value, c_index, r_index, row );
+      } ).slice();
+  } ).slice();
+}
+
+function addConstant( n ) {
+  var f = addConstantIteration.bind(n);
+  return new Matrix(this.iterate(f));
+}
+
+function addConstantIteration(n) {
+  return n + this;
+}
+
+function addMatrix( m ) {
+  var f = addToMatrixIteration.bind(m);
+  return new Matrix(this.iterate(f));
+}
+
+function addToMatrixIteration( v, c, r) {
+  return v + this.at( r, c );
+}
+
+function multiplyConstant( n ) {
+  var f = multiplyConstantIteration.bind(n)
+  return new Matrix(this.iterate(f));
+}
+
+function multiplyConstantIteration( v ) {
+  return v * this;
+}
+
+function multiplyMatrix( m ) {
+  if( this.C !== m.R ) throw new Error('C must equal m.R');
+  var f = multiplyMatrixRowIteration.bind(m);
+  var x = this.rows().map(f).slice();
+  return new Matrix( x );
+}
+
+function multiplyMatrixRowIteration( row_r, r ) {
+  var f = thisRowVectorDotCol.bind(row_r);
+  var x = this.cols().map(f).slice();
+  return x;
+}
+
+function thisRowVectorDotCol( col_c, c ) {
+  var x = vectorDot( this, col_c );
+  return x;
+}
+
+function vectorDot( v1, v2 ) {
+  if( v1.length !== v2.length ) throw new Error('Vectors are not the same size.');
+  var f = vectorDotMapIteration.bind(v2);
+  var x = v1.map(f).reduce(sumReduceIteration);
+  return x;
+}
+
+function vectorDotMapIteration(v, k) {
+  return v * this[k];
+}
+
+function sumReduceIteration(a, i) {
+  a += i;
+  return a;
+}
+
+function concat( m ) {
+  if( this.R !== m.R ) throw new Error('Matrices have different number of rows');
+  var f = concatMapIteration.bind(m);
+  var x = this.rows().map(f).slice();
+  return new Matrix( x );
+}
+
+function concatMapIteration(row_r, r) {
+  return row_r.concat( this.row(r) );
+}
+
+function toString() {
+  var max = Math.max.apply(null, this.flatten());
+  var numDigits = minDigits(max);
+  var f = toStringMatrixReduceIteration.bind({numDigits: numDigits});
+  return this.rows().reduce(f, '');
+}
+
+function toStringMatrixReduceIteration(a, r) {
+  var f = toStringRowReduceIteration.bind(this);
+  return a + r.reduce(f, '\n');
+}
+
+function toStringRowReduceIteration(a, c) {
+  return a + ' ' + padString(c, this.numDigits);
+}
+
+function minDigits(x) {
+  return Math.ceil(Math.log(x) / Math.LN10)
+}
+
+function padString(n, width, x) {
+  x = x || ' ';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(x) + n;
+}
+
+function flatten() {
+  return this.a.reduce(flattenReduceIteration, []);
+}
+
+function flattenReduceIteration(a,x) {
+  return a.concat(x);
+}
+
 module.exports = Matrix;
+
